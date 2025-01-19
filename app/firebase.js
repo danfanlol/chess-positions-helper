c6b18e849fcdef430f831a0e86894362a1ec7584
Daniel <danfanhanyuan@gmail.com>
2025-01-19 15:03:25 -0500

    firebase add-on?

---
+++ app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMw1y_9otOe6n7rvPJn9-1Hnzjf_LhNGs",
+  authDomain: "a-knights-tour.firebaseapp.com",
+  projectId: "a-knights-tour",
+  storageBucket: "a-knights-tour.firebasestorage.app",
+  messagingSenderId: "416706846732",
+  appId: "1:416706846732:web:461d1d4f12af447e85ce29"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
